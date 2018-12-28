691664f3164e3af4780f6f2b33464c9767127857
Joshua Ortmann <joshuaortmann@gmail.com>
2018-12-27 19:57:02 -0500

    InitialCommit: 1. Post-Identity Framework NuGet Installation 2. Pre-initial Migration

---
+++ CCMS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CCMS.Areas.Identity.Data;
+using CCMS.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CCMS.Areas.Identity.IdentityHostingStartup))]
+namespace CCMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CCMSContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CCMSContextConnection")));
+
+                services.AddDefaultIdentity<CCMSUser>()
+                    .AddEntityFrameworkStores<CCMSContext>();
+            });
+        }
+    }
+}
