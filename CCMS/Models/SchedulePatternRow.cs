98bdc82a9b8cb63154f142532f9073daa72b72d5
Joshua Ortmann <joshuaortmann@gmail.com>
2019-05-17 23:14:14 -0400

    Added SchedulePattern and SchedulePatternRow model files

---
+++ CCMS/Models/SchedulePatternRow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCMS.Models
+{
+    public enum DOW : byte
+    {
+        Mon = 0,
+        Tue = 1 << 0,
+        Wed = 1 << 1,
+        Thu = 1 << 2,
+        Fri = 1 << 3,
+        Sat = 1 << 4,
+        Sun = 1 << 5
+    }
+    public class SchedulePatternRow
+    {
+        public long Id { get; set; }
+        public int WeekNumber { get; set; }
+        public DOW DOW { get;set; }
+        public DateTime TimeIn { get; set; }
+        public DateTime TimeOut { get; set; }
+    }
+}
